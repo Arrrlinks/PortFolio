e324914c07e8fc65e914aa15d64bc26444ea729e
Arlinks <45461034+Arrrlinks@users.noreply.github.com>
2023-04-10 18:20:27 +0200

    feat: PortFolio

---
+++ script.js
@@ -0,0 +1,41 @@
+const welcomeName = document.getElementById('welcome-name');
+const lang = document.documentElement.lang
+let words = ['Antoine', 'looking for an internship', 'motivated', 'a student at CESI'];
+if (lang === 'fr') {
+     words = ['Antoine', 'à la recherche d\'un stage', 'motivé', 'étudiant à CESI'];
+}
+let wordIndex = 0;
+let i = words[wordIndex].length;
+let isDeleting = false;
+let speed = 100; // vitesse de suppression et d'ajout des lettres
+let delay = 2000; // délai avant de commencer à effacer le mot
+
+function type() {
+    const currentWord = words[wordIndex];
+    const text = isDeleting ? currentWord.substring(0, i - 1) : currentWord.substring(0, i);
+    welcomeName.textContent = text;
+    if (isDeleting) {
+        i--;
+    } else {
+        i++;
+    }
+    if (i == currentWord.length + 1) {
+        isDeleting = true;
+        speed = 150;
+        delay = 2000;
+    }
+    if (i == 0) {
+        wordIndex = (wordIndex + 1) % words.length;
+        isDeleting = false;
+        speed = 100;
+        delay = 2000;
+    }
+    if (i === currentWord.length+1) {
+        setTimeout(type, delay);
+    }
+    else {
+        setTimeout(type, speed);
+    }
+}
+
+setTimeout(type, delay);
